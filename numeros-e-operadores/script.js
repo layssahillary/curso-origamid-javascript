d61413623e35e7b4cda157b2dc0a3b9b72800d0f
Layssa Hillary <layssahillary@hotmail.com>
2023-05-23 16:54:22 -0300

    numeros e operadores pt1

---
+++ numeros-e-operadores/script.js
@@ -0,0 +1,16 @@
+var total = 10 + 5 + 10;
+var divisao = 200 / 5;
+var modulo = 234444 % 3; 
+
+console.log(total);
+
+var testeNaN = 'Isso é 100' / 2;
+console.log(isNaN(testeNaN));
+
+var soma1 = (10 + 10) + 20 + 30 * 4 / 2 + 10;
+console.log(soma1);
+
+var x = 5;
+console.log(x++);
+console.log(x);
+
